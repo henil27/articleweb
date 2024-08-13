92919caaed503a99e2774475ac06e45ba1ad1fea
henil27 <155354207+henil27@users.noreply.github.com>
2024-08-13 11:35:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+gsap.from("h1", {
+    opacity:0,
+    duration: 1,
+    x:50,
+    delay: 1,
+})
+gsap.from("p", {
+    opacity:0,
+    duration: 1.5,
+    x:-50,
+    delay: 1,
+})
+gsap.from(".home-image-box img", {
+    opacity:0,
+    duration: 1.5,
+    y:-70,
+    delay: 1,
+})
