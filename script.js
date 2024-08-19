60138b846458f3098b6d43963d05b57f892225cf
henil27 <155354207+henil27@users.noreply.github.com>
2024-08-19 22:29:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,34 @@ gsap.from(".home-image-box img", {
     y:-70,
     delay: 1,
 })
+gsap.from(".About-page-box h2", {
+    transform: "translateX(-65%)",
+    scrollTrigger: {
+        trigger: ".About-page-box h2",
+        scroller: "body",
+        start:50,
+        scrub:1
+    }
+})
+gsap.from(".About-page-box #about-info", {
+    transform: "translateX(100%)",
+    duration:2,
+    scrollTrigger: {
+        trigger: ".About-page-box #about-info",
+        scroller: "body",
+        start:50,
+        end:100,
+        scrub:5
+    }
+})
+gsap.from(".Authors-box #author-heading", {
+    transform: "translateY(-95%)",
+    duration:2,
+    scrollTrigger: {
+        trigger: "Authors-box #author-heading",
+        scroller: "body",
+        start:50,
+        end:100,
+        scrub:5
+    }
+})
